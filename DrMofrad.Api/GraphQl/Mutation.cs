6c1e7bad9d9b8091483f0d8cab9a4a4b058268cd
kamal kamel <kamalkss@hotmail.com>
2021-04-23 23:35:52 +0430

    mutation

---
+++ DrMofrad.Api/GraphQl/Mutation.cs
@@ -0,0 +1,33 @@
+﻿using DrMofrad.Api.GraphQl.ActivityCenter;
+using DrMofrad.Api.Model;
+using HotChocolate;
+using HotChocolate.Data;
+using HotChocolate.Subscriptions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DrMofrad.Api.GraphQl
+{
+    public class Mutation
+    {
+        [UseDbContext(typeof(DrMofradDbContext))]
+        public async Task<AddActivityCenterPayLoad> AddActivityCenterAsync(
+            AddActivityCenterInput input,
+            [ScopedService] DrMofradDbContext drMofradDbContext,
+            [Service] ITopicEventSender eventSender,
+            CancellationToken cancellationToken
+        )
+        {
+            var ActivityCenter = new Model.ActivityCenter
+            {
+                Title = input.Title,
+                LangId = input.ActivityCenterLangId,
+                MoreInfo = input.MoreInfo
+            };
+            drMofradDbContext.ActivityCenters.Add(ActivityCenter);
+            await drMofradDbContext.SaveChangesAsync(cancellationToken);
+            await eventSender.SendAsync(nameof(Subscription.OnActivityCenterAdded), ActivityCenter, cancellationToken);
+            return new AddActivityCenterPayLoad(ActivityCenter);
+        }
+    }
+}
---
+++ DrMofrad.Api/GraphQl/Subscription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HotChocolate;
+using HotChocolate.Types;
+
+namespace DrMofrad.Api.GraphQl
+{
+    public class Subscription
+    {
+        [Subscribe]
+        [Topic]
+        public Model.ActivityCenter OnActivityCenterAdded([EventMessage] Model.ActivityCenter activityCenter)
+        {
+            return activityCenter;
+        }
+    }
+}
